d23a5809d8dcf3d7511122ad48d281df1894014f
Joaquimfl8 <160670206+Joaquimfl8@users.noreply.github.com>
2026-03-12 15:45:15 -0300

    Update exercicio8.py

--- exercicio8.py
+++ exercicio8.py
@@ -1,6 +1,8 @@
 nota: int = int(input("Digite sua nota: "))
 
-if nota >= 7:
+if nota >= 7 and nota <= 10:
     print("Aprovado!")
-else:
+elif nota >= 0 and nota <= 7:
     print("Reprovado!")
+else:
+    print("Nota inválida")
